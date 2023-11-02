c80e8a8195b865e6d9d87809b54e203d9a2b1a7b
Evgeniy Bulochnikov <evgenie_1994@mail.ru>
2023-11-02 20:54:35 +0300

    HW8 +

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        const restore = restoreState<string>('hw6-editable-span-value', value)
+        setValue(restore)
     }
 
     return (
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
 
-            return state // need to fix
+        case 'sort': { // by name
+            return action.payload === 'up' ?
+                [...state].sort((a, b) => (a.name > b.name ? 1 : -1))
+                : [...state].sort((a, b) => (a.name < b.name ? 1 : -1))
+            // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(a => a.age > 18) // need to fix
         }
         default:
             return state
